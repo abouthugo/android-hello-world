7a5d1a7c0338bf63c3bf6090d6cc708d4a4b034a
Hugo Perdomo <hugo@Hugos-Mac18.home>
2019-02-03 23:07:46 -0500

    Added toast

--- app/src/main/java/com/example/greetings/MainActivity.java
+++ app/src/main/java/com/example/greetings/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.greetings;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private CharSequence text = "Greetings!";
+    int duration = Toast.LENGTH_SHORT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast toast = Toast.makeText(this, text, duration);
         setContentView(R.layout.activity_main);
+        toast.show();
     }
 }
